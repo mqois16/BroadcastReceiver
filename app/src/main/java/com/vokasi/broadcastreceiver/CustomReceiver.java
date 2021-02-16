d655fab2716ea2ab77c62c7b178b13222b709ffc
mqois16 <qoisoctava@gmail.com>
2021-02-16 21:02:05 +0700

    Melengkapi

--- app/src/main/java/com/vokasi/broadcastreceiver/CustomReceiver.java
+++ app/src/main/java/com/vokasi/broadcastreceiver/CustomReceiver.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
-
+    private static  final String ACTION_CUSTOM_BROADCAST=BuildConfig.APPLICATION_ID+".ACTION_CUSTOM_BROADCAST";
     @Override
     public void onReceive(Context context, Intent intent) {
         String intentAction=intent.getAction();
@@ -19,7 +19,9 @@ public class CustomReceiver extends BroadcastReceiver {
                 case Intent.ACTION_POWER_DISCONNECTED:
                     toastMessage=context.getString(R.string.power_disconnected);
                     break;
-
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage=intent.getStringExtra("DATA");
+                    break;
             }
             Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show();
         }
