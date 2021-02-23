d3c64412597fa6a1ad43ef0c7aef37b27eecd5de
mqois16 <qoisoctava@gmail.com>
2021-02-23 19:49:21 +0700

    Pertemuan 3 / WidgetApp

--- app/src/main/java/com/vokasi/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/vokasi/broadcastreceiver/MainActivity.java
@@ -1,6 +1,7 @@
 package com.vokasi.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -9,6 +10,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private  CustomReceiver mReceiver= new CustomReceiver();
+    private static  final String ACTION_CUSTOM_BROADCAST=BuildConfig.APPLICATION_ID+".ACTION_CUSTOM_BROADCAST";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +21,19 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         this.registerReceiver(mReceiver,filter);
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver,new IntentFilter(ACTION_CUSTOM_BROADCAST));
     }
 
     @Override
     protected void onDestroy() {
         this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
         super.onDestroy();
     }
 
     public void sendCustomBroadcast(View view) {
+            Intent customBroadcastIntent=new Intent(ACTION_CUSTOM_BROADCAST);
+            customBroadcastIntent.putExtra("DATA","Data Broadcast");
+            LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
     }
 }
